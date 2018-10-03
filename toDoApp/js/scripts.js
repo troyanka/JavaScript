5e17a85000547ce887635506936151eaaac2c6e6
troyanka <troyanka@gmail.com>
2018-10-03 15:30:01 +0300

    update

--- toDoApp/js/scripts.js
+++ toDoApp/js/scripts.js
@@ -1,18 +1,47 @@
+function init (){
+   if(localStorage.getItem('tasks') !== null){
+    var existingTasks = JSON.parse(localStorage.getItem('tasks'));
+    
+    existingTasks.forEach(function(task) {
+       showTask(task.taskTimeStamp, task.taskTitle, task.taskDescription);
+    });
+   }
+}
+init();
+
 function saveToLS(taskTitle, taskDescription) {
    var taskTimeStamp = new Date().getTime();
    console.log(taskTimeStamp);
     
    var isInLS = JSON.parse(localStorage.getItem('tasks'));
+   
    if(isInLS != null){
-    console.log('there are notes already');
+    var existingTasks = isInLS;
+    existingTasks.push( { taskTimeStamp, taskTitle, taskDescription } );
+    localStorage.setItem('tasks', JSON.stringify(existingTasks) );
    }
    else{
-    console.log('there in no notes');
-    
     var newTask = [];
     newTask.push( { taskTimeStamp, taskTitle, taskDescription } );
     localStorage.setItem('tasks', JSON.stringify(newTask) );
    }
+}
+
+function showTask(taskTimeStamp, taskTitle, taskDescription){
+  var tasksDesc = document.querySelector('.existing-tasks');
+  var taskDiv = document.createElement('div');
+  taskDiv.className = 'task';
+  taskDiv.setAttribute("data-note-id", taskTimeStamp);
+
+  var taskHeader = document.createElement('h1');
+  taskHeader.innerHTML = taskTitle;
+
+  var taskDesc = document.createElement('p');
+  taskDesc.innerHTML = taskDescription;
+
+  taskDiv.appendChild(taskHeader);
+  taskDiv.appendChild(taskDesc);
+  tasksDesc.appendChild(taskDiv);
 }
 
 var addButton = document.querySelector('.add-task'); 
@@ -21,4 +50,5 @@ addButton.addEventListener("click", function () {
     var taskDescription = document.querySelector('.task-description').value;
 
       saveToLS(taskTitle, taskDescription);
+      //showTask();
   })
