5712d796563c568865e8e69b3a18a33d4186b3b5
troyanka <troyanka@gmail.com>
2018-10-04 12:01:30 +0300

    update

--- toDoApp/js/scripts.js
+++ toDoApp/js/scripts.js
@@ -9,46 +9,82 @@ function init (){
 }
 init();
 
-function saveToLS(taskTitle, taskDescription) {
+function saveToLS(taskTitle, taskDescription, taskDueDate) {
    var taskTimeStamp = new Date().getTime();
-   console.log(taskTimeStamp);
-    
    var isInLS = JSON.parse(localStorage.getItem('tasks'));
    
    if(isInLS != null){
     var existingTasks = isInLS;
-    existingTasks.push( { taskTimeStamp, taskTitle, taskDescription } );
+    existingTasks.push( { taskTimeStamp, taskTitle, taskDescription, taskDueDate } );
     localStorage.setItem('tasks', JSON.stringify(existingTasks) );
    }
    else{
     var newTask = [];
-    newTask.push( { taskTimeStamp, taskTitle, taskDescription } );
+    newTask.push( { taskTimeStamp, taskTitle, taskDescription, taskDueDate } );
     localStorage.setItem('tasks', JSON.stringify(newTask) );
    }
+
+   return taskTimeStamp;
 }
 
-function showTask(taskTimeStamp, taskTitle, taskDescription){
+function removeTask(event) {
+      var taskToDeleteId = event.target.parentElement.parentElement.dataset.noteId;
+      var existingTasks = JSON.parse(localStorage.getItem('tasks'));
+      var indexToDelete = existingTasks.findIndex(obj => obj.taskTimeStamp == taskToDeleteId);
+      existingTasks.splice(indexToDelete, 1);
+      localStorage.setItem('tasks', JSON.stringify(existingTasks) );
+      event.target.parentElement.parentElement.remove();
+}
+
+function showTask(taskTimeStamp, taskTitle, taskDescription, taskDueDate ){
   var tasksDesc = document.querySelector('.existing-tasks');
+  
+  //main div
   var taskDiv = document.createElement('div');
-  taskDiv.className = 'task';
+  taskDiv.className = 'task card border-info';
   taskDiv.setAttribute("data-note-id", taskTimeStamp);
 
-  var taskHeader = document.createElement('h1');
+  //task body
+  var taskBody = document.createElement('div');
+  taskBody.className = 'card-body';
+  
+  //task title
+  var taskHeader = document.createElement('div');
   taskHeader.innerHTML = taskTitle;
+  taskHeader.className = 'card-header';
 
+ //task due date
+  var taskDate = document.createElement('p');
+  taskDate.innerHTML = taskDueDate;
+  taskDate.className = 'card-text';
+
+  //task description text
   var taskDesc = document.createElement('p');
   taskDesc.innerHTML = taskDescription;
+  taskDesc.className = 'card-text task-due-date';
 
-  taskDiv.appendChild(taskHeader);
-  taskDiv.appendChild(taskDesc);
-  tasksDesc.appendChild(taskDiv);
+  //delete button 
+    var deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete task';
+    deleteButton.className="btn btn-primary delete-task";
+    deleteButton.addEventListener('click', removeTask);
+    
+  //append created task divs to the panel
+    taskDiv.appendChild(taskHeader);
+    taskBody.appendChild(taskDate);
+    taskBody.appendChild(taskDesc);
+    taskBody.appendChild(deleteButton);
+    taskDiv.appendChild(taskBody);
+    tasksDesc.appendChild(taskDiv);
 }
 
-var addButton = document.querySelector('.add-task'); 
-addButton.addEventListener("click", function () {
+    var addButton = document.querySelector('.add-task'); 
+    addButton.addEventListener("click", function () {
     var taskTitle = document.querySelector('.task-title').value;
     var taskDescription = document.querySelector('.task-description').value;
+    var taskDueDate = document.querySelector('.task-due-date').value;
+    console.log(taskDueDate);
 
-      saveToLS(taskTitle, taskDescription);
-      //showTask();
+    var taskTimeStamp = saveToLS(taskTitle, taskDescription, taskDueDate);
+    showTask(taskTimeStamp, taskTitle, taskDescription, taskDueDate);
   })
